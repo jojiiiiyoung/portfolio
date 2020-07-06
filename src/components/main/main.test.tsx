0973cd1233ef7d0cc425426815029a4f608cd5d0
jojiiiiyoung <jojiiiiyoung@gmail.com>
2020-07-06 19:01:56 +0900

    Fix break tests

--- src/components/main/main.test.tsx
+++ src/components/main/main.test.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Main from './';
-import { shallow } from 'enzyme';
-import TypingWords from './typing-words';
+/* eslint-disable @typescript-eslint/no-var-requires */
+import React from "react";
+import { render } from "@testing-library/react";
+import { shallow } from "enzyme";
+import Main from ".";
 
+const RotatingText = require("react-rotating-text");
 
-describe('contains proper text', () => {
+describe("contains proper text", () => {
   const { getByText } = render(<Main />);
 
   it('main page includes "jiyoung" text', () => {
@@ -14,10 +15,13 @@ describe('contains proper text', () => {
   });
 });
 
-describe('renders main page normally', () => {
+describe("renders main page normally", () => {
   const wrapper = shallow(<Main />);
-
-  it('main page includes TypingWords component', () => {
-    expect(wrapper.contains(<TypingWords words={['Frontend Developer', 'working in SK planet.']} />)).toBeTruthy();
+  it("main page includes TypingWords component", () => {
+    expect(
+      wrapper.contains(
+        <RotatingText items={["Frontend Developer", "working in SK planet."]} />
+      )
+    ).toBeTruthy();
   });
 });
