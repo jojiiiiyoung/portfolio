dd102e3f350761f082d0b520577fda4a660912f1
jojiiiiyoung <jojiiiiyoung@gmail.com>
2020-07-06 18:00:19 +0900

    Add hook to detect visibility

---
+++ src/components/common/useVisible.ts
@@ -0,0 +1,34 @@
+import { useState, useCallback, useEffect, MutableRefObject } from "react";
+
+const checkVisible = (el: HTMLElement | null): boolean => {
+  if (!el) {
+    return false;
+  }
+
+  const { innerHeight } = window;
+  const { bottom, top } = el.getBoundingClientRect();
+
+  return top <= innerHeight && bottom >= 0;
+};
+
+const useVisible = (
+  el: MutableRefObject<HTMLDivElement | null>
+): { visible: boolean } => {
+  const [visible, setVisible] = useState<boolean>(false);
+
+  const handleScroll = useCallback((): void => {
+    setVisible(checkVisible(el.current));
+  }, [el]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return (): void => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [handleScroll, el]);
+
+  return { visible };
+};
+
+export default useVisible;
