9583ded068af49bb520528af76603b5100d8aa1f
jojiiiiyoung <jojiiiiyoung@gmail.com>
2020-07-21 10:15:27 +0900

    Add animation to icons in contact section

--- src/components/contact/index.tsx
+++ src/components/contact/index.tsx
@@ -1,9 +1,12 @@
 import React, { useRef } from "react";
+import { Lottie } from "@crello/react-lottie";
 
-import "./style.css";
-import { IMAGE_URL } from "../../constants";
 import useHash from "../common/useHash";
 import useVisibility from "../common/useVisibility";
+
+import linkedIn from "./linkedin.json";
+import mail from "./email.json";
+import "./style.css";
 
 const Contact = (): React.ReactElement => {
   const containerEl = useRef<HTMLDivElement | null>(null);
@@ -17,14 +20,33 @@ const Contact = (): React.ReactElement => {
         <h3>Contact Me</h3>
         <div className="sns-list">
           <a
+            className="contact-link"
             target="_blank"
             rel="noopener noreferrer"
             href="https://www.linkedin.com/in/jiyoung-jo-17b882b1"
           >
-            <img src={`${IMAGE_URL}linked-in.png`} alt="linked in" />
+            <Lottie
+              config={{
+                loop: true,
+                autoplay: true,
+                animationData: linkedIn,
+              }}
+              height="50px"
+              width="50px"
+            />
+            {/* <img src={`${IMAGE_URL}linked-in.png`} alt="linked in" /> */}
           </a>
-          <a href="mailto:jojiiiiyoung@gmail.com">
-            <img src={`${IMAGE_URL}mail.png`} alt="mail" />
+          <a className="contact-link" href="mailto:jojiiiiyoung@gmail.com">
+            <Lottie
+              config={{
+                loop: true,
+                autoplay: true,
+                animationData: mail,
+              }}
+              height="50px"
+              width="50px"
+            />
+            {/* <img src={`${IMAGE_URL}mail.png`} alt="mail" /> */}
           </a>
         </div>
       </div>
